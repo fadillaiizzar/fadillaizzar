d6037077481651c2633cb7971f03c5e9d181f6ee
fadillaiizzar <fadillaiizzarm@gmail.com>
2025-03-06 16:49:03 +0700

    box image find

--- src/components/Elements/SectProfile.jsx/ImgFind.jsx
+++ src/components/Elements/SectProfile.jsx/ImgFind.jsx
@@ -2,7 +2,7 @@ export default function ImgFind(props) {
     const { link , arialabel , imgFind , alt } = props;
     
     return(
-        <a href={link} aria-label={`${arialabel}`} target="blank">
+        <a href={link} aria-label={`${arialabel}`} target="blank" className="bg-cbeige mr-2 rounded-lg">
             <img src={imgFind} className="w-12 h-12" alt={`${alt}`} />
         </a>
     );
